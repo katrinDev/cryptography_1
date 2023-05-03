33123e8d541f67882aa33fb2f93da800b495e78e
Vlasov Andrey <andreivlasov11@gmail.com>
2023-05-03 19:29:44 +0300

    comments

--- source/components/LSB.tsx
+++ source/components/LSB.tsx
@@ -10,46 +10,59 @@ export const LSB: FC<{
 	const [keyString, setKeyString] = useState<string>("");
 
 	async function handleEncrypt(keyString: string) {
+		// Читаем данные из файла input.mp3
 		const data = await readFile("input.mp3");
 
-		// Convert message to binary
+		// Преобразуем сообщение в двоичное представление
 		const binaryMessage = keyString
 			.split("")
 			.map((char) => char.charCodeAt(0).toString(2).padStart(8, "0"))
 			.join("");
 
-		// Embed message using LSB method
+		// Встраиваем сообщение с помощью метода LSB
 		let dataIndex = 0;
 		for (let i = 0; i < binaryMessage.length; i += 3) {
+			// Извлекаем три бита из двоичного сообщения
 			const bitsToEmbed = binaryMessage.slice(i, i + 3);
+
+			// Встраиваем три бита в текущий байт данных
 			data[dataIndex] = (data[dataIndex] & ~7) | parseInt(bitsToEmbed, 2);
+
+			// Переходим к следующему байту данных
 			dataIndex++;
 		}
 
-		// Save output file
+		// Сохраняем измененные данные в файл output.mp3
 		await writeFile("output.mp3", data);
+
+		// Вызываем функцию дешифровки для проверки результата
 		await handleDecrypt(keyString.length);
+
+		// Очищаем значения выбранных элементов
 		setSelectedItem("");
 		setEncryptedItem("");
 	}
 
 	async function handleDecrypt(messageLength: number): Promise<void> {
+		// Читаем данные из файла output.mp3
 		const data = await readFile("output.mp3");
 
-		// Extract message using LSB method
+		// Извлекаем сообщение с помощью метода LSB
 		let binaryMessage = "";
 		for (let i = 0; i < messageLength * 8; i += 3) {
+			// Извлекаем три бита из текущего байта данных
 			binaryMessage += (data[Math.floor(i / 3)] & 7)
 				.toString(2)
 				.padStart(3, "0");
 		}
 
-		// Convert binary to string
+		// Преобразуем двоичное представление в строку
 		const message = binaryMessage
 			.match(/.{1,8}/g)
 			?.map((binary) => String.fromCharCode(parseInt(binary, 2)))
 			.join("");
 
+		// Выводим расшифрованное сообщение в консоль
 		console.log("decrypted: ", message);
 	}
 
